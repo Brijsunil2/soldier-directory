846c448da98cdaa9b4dfea802ab4af491bfc77f8
Sunil Brijlall <brijsunil2@gmail.com>
2025-11-07 20:33:43 -0500

    feat(Directory): add working directory listing

--- src/components/Directory/Directory.jsx
+++ src/components/Directory/Directory.jsx
@@ -1,14 +1,34 @@
+import { Fragment, useEffect, useState } from "react";
 import "./Directory.css";
 import DirectoryListing from "./DirectoryListing";
+import { sortDirectoryByName } from "../../utils/sortByName";
 
-const Directory = ({directoryData}) => {
+const Directory = ({ directoryData }) => {
+  const [data, setData] = useState(directoryData);
+
+  useEffect(() => {
+    setData(sortDirectoryByName(directoryData));
+  }, [directoryData]);
+
   return (
     <section className="directory">
-      {directoryData.map((data, index) => (
-        <DirectoryListing key={index} listingData={data} />
+      {data.map((listing, index) => (
+        <Fragment key={index}>
+          {listing.name &&
+            (index === 0 ||
+              data[index - 1]?.name?.charAt(0) !== listing.name.charAt(0)) && (
+              <h2
+                className="directory-letter-header"
+              >
+                {listing.name.charAt(0)}
+              </h2>
+            )}
+
+          <DirectoryListing listingData={listing} />
+        </Fragment>
       ))}
     </section>
-  )
-}
+  );
+};
 
-export default Directory
+export default Directory;
