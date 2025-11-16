3374a9f95f567071b7fa22e838e14a40ec9740c9
Sunil Brijlall <brijsunil2@gmail.com>
2025-11-15 20:48:10 -0500

    refactor(DirectoryListing.jsx): add more data to listings

--- src/components/Directory/DirectoryListing.jsx
+++ src/components/Directory/DirectoryListing.jsx
@@ -1,13 +1,27 @@
-const DirectoryListing = ({listingData}) => {
-  
+const DirectoryListing = ({ listingData }) => {
   return (
     <div className="directory-listing">
+      <div className="listing-image-container">
+        {listingData.imageUrl ? (
+          <img
+            src={listingData.imageUrl}
+            alt={listingData.name}
+            className="listing-image"
+          />
+        ) : (
+          <div className="listing-image-placeholder">No Image</div>
+        )}
+      </div>
       <a href={listingData.link} target="_blank" rel="noopener noreferrer">
         <p className="listing-name">{listingData.name}</p>
       </a>
-      {/* <p className="listing-data">{listingData.description}</p> */}
+      <p className="listing-subtext">
+        Service Number: {listingData.serviceNumber}
+      </p>
+      <p className="listing-description">{listingData.description}</p>
+      <p className="listing-link">View Profile</p>
     </div>
-  )
-}
+  );
+};
 
-export default DirectoryListing
+export default DirectoryListing;
