4739c53ae309780935aed96fcfe1add89bfbf640
Sunil Brijlall <brijsunil2@gmail.com>
2025-11-16 20:55:45 -0500

    style(Directory): add new card style to directory listings

--- src/components/Directory/DirectoryListing.jsx
+++ src/components/Directory/DirectoryListing.jsx
@@ -1,25 +1,28 @@
 const DirectoryListing = ({ listingData }) => {
   return (
     <div className="directory-listing">
-      <div className="listing-image-container">
-        {listingData.imageUrl ? (
-          <img
-            src={listingData.imageUrl}
-            alt={listingData.name}
-            className="listing-image"
-          />
-        ) : (
-          <div className="listing-image-placeholder">No Image</div>
-        )}
-      </div>
       <a href={listingData.link} target="_blank" rel="noopener noreferrer">
-        <p className="listing-name">{listingData.name}</p>
+        <div className="listing-image-container">
+          {listingData.imageUrl ? (
+            <img
+              src={listingData.imageUrl}
+              alt={listingData.name}
+              className="listing-image"
+            />
+          ) : (
+            <div className="listing-image-placeholder">No Image</div>
+          )}
+        </div>
+
+        <h4 className="listing-name">{listingData.name}</h4>
+
+        <p className="listing-subtext">
+          Service No. {listingData.serviceNumber}
+        </p>
+        <p className="listing-description">{listingData.description}</p>
+
+        <p className="listing-link">View Profile</p>
       </a>
-      <p className="listing-subtext">
-        Service Number: {listingData.serviceNumber}
-      </p>
-      <p className="listing-description">{listingData.description}</p>
-      <p className="listing-link">View Profile</p>
     </div>
   );
 };
